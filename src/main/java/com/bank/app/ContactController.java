fe0bbe27fbfabb3237ecbeff2e63f0b1c9ea3cb5
eholmes83 <eholmes83@yahoo.com>
2026-03-02 15:47:35 -0500

    initial commit + spring security lab

---
+++ src/main/java/com/bank/app/ContactController.java
@@ -0,0 +1,32 @@
+package com.bank.app;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/contacts")
+public class ContactController {
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+    public String getContacts() {
+        return "Returning all contacts";
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public String addContact() {
+        return "New contact added!";
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String deleteContact(@PathVariable int id) {
+        return "Contact " + id + " deleted!";
+    }
+
+    @GetMapping("/public/info")
+    public String publicInfo() {
+        return "This is a public endpoint";
+    }
+}
---
+++ src/main/java/com/bank/app/SecurityConfig.java
@@ -0,0 +1,69 @@
+package com.bank.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authRequests) -> authRequests
+                .requestMatchers("/contacts/public/**").permitAll()
+                .requestMatchers(HttpMethod.GET,"/contacts").hasAnyRole("USER", "ADMIN")
+                .requestMatchers(HttpMethod.POST,"/contacts").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE,"/contacts/**").hasRole("ADMIN")
+                .anyRequest().authenticated());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.headers(headers -> headers
+                .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin
+                )
+        );
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.httpBasic(withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder().encode("user123"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
